4d18c3a237c47dafb43359c969286a7ceecd4cdd
gentwocoder <adetoyese0511@gmail.com>
2025-07-20 23:02:50 +0100

    enhance ride request feature with improved Leaflet map initialization and location suggestions

--- rideshare/rideon/admin.py
+++ rideshare/rideon/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ride, RideRequest, RideMessage
 
 # Register your models here.
+admin.site.register(Ride)
+admin.site.register(RideRequest)
+admin.site.register(RideMessage)
+admin.site.site_header = "RideOn Admin"
+admin.site.site_title = "RideOn Admin Portal"
+admin.site.index_title = "Welcome to the RideOn Admin Portal"
